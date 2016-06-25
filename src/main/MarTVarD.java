1f4d2ad55280019e977386131683bb212097d50d
LeeLoongKuan <kennylklee48@gmail.com>
2016-06-25 17:03:14 +1000

    Fix logic error when creating output table

--- src/main/MarTVarD.java
+++ src/main/MarTVarD.java
@@ -116,8 +116,8 @@ public class MarTVarD {
             results[i][6] = "";
             results[i][7] = "";
             for (int j = 0; j < this.orderedNple[i].length; j++) {
-                results[i][4] += "_" + this.orderedNple[i][j] + "=" + this.localMax[i][1+j];
-                results[i][6] += "_" + this.orderedNple[i][j] + "=" + this.localMin[i][1+j];
+                results[i][4] += "_" + this.orderedNple[i][j] + "=" + this.localMax[i][j];
+                results[i][6] += "_" + this.orderedNple[i][j] + "=" + this.localMin[i][j];
                 results[i][7] += "_" + this.orderedNple[i][j];
             }
         }
